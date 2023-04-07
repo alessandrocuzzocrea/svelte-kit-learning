d6b7332031f3ab1f18e7937c25197e280672db6d
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2023-04-08 01:25:54 +0900

    Remove unused stuff

--- src/lib/components/PostEditor.svelte
+++
@@ -1,7 +0,0 @@
-<form method="POST" action="/pocketbase">
-  <label>
-    src/lib/components
-    <input name="body" type="text" />
-  </label>
-  <button>Post New Message</button>
-</form>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,71 +3,68 @@
   import Player from '../components/Player.svelte';
 </script>
 
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link
-      href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.16/dist/tailwind.min.css"
-      rel="stylesheet"
-    />
-    <title>Sidebar Layout</title>
-  </head>
-  <body class="bg-gray-200">
-    <div class="flex h-screen">
-      <!-- Sidebar -->
-      <div class="bg-blue-600 w-64 flex flex-col p-4 text-white">
-        <div class="mb-8">
-          <h1 class="text-2xl font-semibold mb-2">Brand</h1>
-          <span class="text-sm">Your tagline or description</span>
-        </div>
-        <nav>
-          <ul class="space-y-2">
-            <li>
-              <a
-                href="#"
-                class="flex items-center space-x-2 py-2 px-3 rounded-lg hover:bg-blue-700 transition-colors"
-              >
-                <!-- <span class="material-icons">dashboard</span> -->
-                <span>For You</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="flex items-center space-x-2 py-2 px-3 rounded-lg hover:bg-blue-700 transition-colors"
-              >
-                <!-- <span class="material-icons">inbox</span> -->
-                <span>Browse</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="flex items-center space-x-2 py-2 px-3 rounded-lg hover:bg-blue-700 transition-colors"
-              >
-                <!-- <span class="material-icons">people</span> -->
-                <span>Project 1</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="flex items-center space-x-2 py-2 px-3 rounded-lg hover:bg-blue-700 transition-colors"
-              >
-                <!-- <span class="material-icons">settings</span> -->
-                <span>Project 2</span>
-              </a>
-            </li>
-          </ul>
-        </nav>
+<body class="bg-gray-200">
+  <div class="flex h-screen">
+    <!-- Sidebar -->
+    <div class="bg-blue-600 w-64 flex flex-col p-4 text-white">
+      <div class="mb-8">
+        <h1 class="text-2xl font-semibold mb-2">Brand</h1>
+        <span class="text-sm">Your tagline or description</span>
       </div>
-      <!-- Content -->
-      <div class="flex-1 flex-col bg-white">
-        <Player />
-        <slot />
-      </div>
+      <nav>
+        <ul class="space-y-2">
+          <li>
+            <a
+              href="/"
+              class="flex items-center space-x-2 py-2 px-3 rounded-lg hover:bg-blue-700 transition-colors"
+            >
+              <!-- <span class="material-icons">dashboard</span> -->
+              <span>Home</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              class="flex items-center space-x-2 py-2 px-3 rounded-lg hover:bg-blue-700 transition-colors"
+            >
+              <!-- <span class="material-icons">dashboard</span> -->
+              <span>For You</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="/browse"
+              class="flex items-center space-x-2 py-2 px-3 rounded-lg hover:bg-blue-700 transition-colors"
+            >
+              <!-- <span class="material-icons">inbox</span> -->
+              <span>Browse</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              class="flex items-center space-x-2 py-2 px-3 rounded-lg hover:bg-blue-700 transition-colors"
+            >
+              <!-- <span class="material-icons">people</span> -->
+              <span>Project 1</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              class="flex items-center space-x-2 py-2 px-3 rounded-lg hover:bg-blue-700 transition-colors"
+            >
+              <!-- <span class="material-icons">settings</span> -->
+              <span>Project 2</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
-  </body>
-</html>
+    <!-- Content -->
+    <div class="flex-1 flex-col bg-white">
+      <Player />
+      <slot />
+    </div>
+  </div>
+</body>
