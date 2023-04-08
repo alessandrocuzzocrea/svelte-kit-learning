b36e0a35e9d5489f3fd47bdc6b639e8cf00a2989
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2023-04-08 11:35:04 +0900

    wip-2

--- src/components/Player.svelte
+++ src/components/Player.svelte
@@ -1,22 +1,25 @@
 <script>
-  import { onMount } from 'svelte';
-
   let isPlaying = false;
-  let timer = 0;
+  let elapsedTime = 0;
+  let clear;
+
   function handleToggleClass() {
     isPlaying = !isPlaying;
 
     if (isPlaying) {
-      // Start the timer
-      timer = setInterval(() => {
-        console.log('Timer tick');
-      }, 1000);
+      clear = setInterval(() => (elapsedTime += 1), 1000);
     } else {
-      // Stop the timer
-      clearInterval(timer);
-      timer = 0;
+      elapsedTime = 0;
+      clearInterval(clear);
     }
   }
+
+  $: hours = String(Math.floor(elapsedTime / 3600)).padStart(2, '0');
+  $: minutes = String(Math.floor(elapsedTime / 60)).padStart(2, '0');
+  $: seconds = String(Math.floor(elapsedTime - Math.floor(elapsedTime / 60) * 60)).padStart(2, '0');
+
+  $: buttonLabel = isPlaying ? 'Stop' : 'Start';
+  $: buttonClass = isPlaying ? 'bg-red-500 hover:bg-red-700' : 'bg-blue-500 hover:bg-blue-700';
 </script>
 
 <div class="flex flex-row items-center p-2 justify-between bg-slate-200">
@@ -26,16 +29,15 @@
     placeholder="What are you doing?"
   />
   <select class="p-1 mr-2">
-    <option>Project 1</option>
-    <option />
+    <option>Task A</option>
+    <option>Task B</option>
     <option>Maybe</option>
   </select>
-  <p class="mr-2">Project</p>
-  <p class="mr-2">00:00:00</p>
+  <p class="mr-2">Project A</p>
+  <p class="mr-2">|</p>
+  <p class="mr-2">{hours}:{minutes}:{seconds}</p>
   <button
     on:click={handleToggleClass}
-    class={`${
-      isPlaying ? 'bg-red-500 hover:bg-red-700' : 'bg-blue-500 hover:bg-blue-700'
-    }  text-white font-bold py-2 px-4 rounded p-1`}>Start</button
+    class={`${buttonClass} text-white font-bold py-2 px-4 rounded p-1`}>{buttonLabel}</button
   >
 </div>
